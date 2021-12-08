bf3ebdf0d245d25d09a3ded624d90fa95d152648
ssexec <vasnev2016@yandex.ru>
2021-12-08 12:57:06 +0300

    5 kurs

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 from prettytable import PrettyTable
+
 mytable = PrettyTable()
-mytable.field_names = ['Фамилия', 'Имя', 'Отчество', 'Дата рождения', 'Балл при поступлении', 'Курс', 'Группа', 'Средний балл']
-aim=[]
-ism=[]
+mytable.field_names = ['Фамилия', 'Имя', 'Отчество', 'Дата рождения', 'Балл при поступлении', 'Курс', 'Группа',
+                       'Средний балл']
+aim = []
+ism = []
 
-aif=open('AI_students.txt','r')
-isf=open('IS_students.txt','r')
+aif = open('AI_students.txt', 'r')
+isf = open('IS_students.txt', 'r')
 
-lines0=aif.read()
-lines1=isf.read()
+lines0 = aif.read()
+lines1 = isf.read()
 
-lines0=lines0.split('\n')
+lines0 = lines0.split('\n')
 for x in lines0:
-  aim.append(x.split())
+    aim.append(x.split())
 
-lines1=lines1.split('\n')
+lines1 = lines1.split('\n')
 for x in lines1:
-  ism.append(x.split())
+    ism.append(x.split())
 
-students=aim+ism
+students = aim + ism
+
+
 def sortbySurname(inputStr):
-  return inputStr[0]
-def sortbySpecialn(inputStr):
-  return inputStr[-2]
+    return inputStr[0]
 
-students.sort(key = sortbySurname)
-students.sort(key = sortbySpecialn)
+
+def sortbySpecialn(inputStr):
+    return inputStr[-2]
+
+
+students.sort(key=sortbySurname)
+students.sort(key=sortbySpecialn)
 
 mytable.add_rows(students)
 #print(mytable)
@@ -40,3 +47,4 @@ for x in pure_guys:
 print(pure_guys)
 puretable=PrettyTable()
 puretable.field_names = ['Фамилия', 'Имя', 'Отчество', 'Дата рождения', 'Специальность', 'Группа', 'Средний балл']
+print(mytable)
