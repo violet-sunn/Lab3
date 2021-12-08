79901a790368797e22c90870ed12882f0f9575bd
SirPelmesh <ketrinquli@gmail.com>
2021-12-08 13:22:24 +0300

    2 task.1

--- main.py
+++ main.py
@@ -36,15 +36,32 @@ students.sort(key=sortbySpecialn)
 
 mytable.add_rows(students)
 #print(mytable)
-
+import random
+import math
 pure_guys=[]
 for x in students:
   if x[-3]=='5':
+    del x[4]
+    x[-3]=x[-2]
     pure_guys.append(x)
+num_gr=math.ceil(len(pure_guys)/5)
+num=0
+k=0
 for x in pure_guys:
-  del x[4]
-  del x[4]
+    if k==5 and num<num_gr:
+        k=0
+        num+=1
+    if k<5:
+        x[-2] = i
+
+
+"""for x in pure_guys:
+    k = 0
+    for i in range (num_gr):
+        while k<4:
+            x[-2]=i
+            k+=1
+"""
 print(pure_guys)
 puretable=PrettyTable()
 puretable.field_names = ['Фамилия', 'Имя', 'Отчество', 'Дата рождения', 'Специальность', 'Группа', 'Средний балл']
-print(mytable)
